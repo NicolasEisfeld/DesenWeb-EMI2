ce89a714de7424b19123163c30e3aa775631c571
NicolasEisfeld <96309633+NicolasEisfeld@users.noreply.github.com>
2025-03-25 11:52:28 -0300

    Add files via upload

---
+++ Exercícios/3Exercicio/script.js
@@ -0,0 +1,12 @@
+const texto = "Olá, mundo!"
+let mensagem = "Primeiro JS"
+let idade = 16
+let altura = 1.74
+var valor = 1234
+document.write(mensagem + "<br>")
+document.write("altura: " + altura + "<br>")
+document.write("idade: " + idade + "<br>")
+document.write("valor: " + valor)
+
+
+
